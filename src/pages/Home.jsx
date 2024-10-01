095d16de559e067564e05af48faa0e5ecdb2cdf5
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2024-10-01 15:32:36 +0200

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,18 @@ import { NavLink } from "react-router-dom";
 import bookImage from "../img/2.jpg";
 export default function Home() {
   return (
-    <div>
-      <div className={css.home_div}>
-        <h1 className={css.h1_home}>Discover and connect with</h1>
-        <p className={css.p_home}>Efficientily search and manage contact</p>
-        <div>
+    <div className={css.container}>
+      <div className={css.content}>
+        <div className={css.text_block}>
+          <h1 className={css.h1_home}>Discover and connect with</h1>
+          <p className={css.p_home}>Efficiently search and manage contact</p>
           <NavLink className={css.link} to="/register">
             Register
           </NavLink>
         </div>
-      </div>
-      <div className={css.img_box}>
-        <img src={bookImage} alt="book" className={css.img} />
+        <div className={css.img_box}>
+          <img src={bookImage} alt="book" className={css.img} />
+        </div>
       </div>
       <div className={css.quotation}>
         <h3>
